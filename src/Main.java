1f07b04c12952f070841bb37260668726be1f066
maxjwebber <tua83483@temple.edu>
2020-01-27 16:47:18 -0500

    all methods and classes implemented

--- src/Main.java
+++ src/Main.java
@@ -17,17 +17,35 @@ public class Main {
         int eside = scan.nextInt();
         scan.close();
 
-        Circle circle = new Circle("circle");
+        Circle circle = new Circle("Circle");
         circle.setDimensions(radius);
-        Square square = new Square("square");
+        Square square = new Square("Square");
         square.setDimensions(squareSide,squareSide);
-        Triangle triangle = new Triangle("triangle");
+        Triangle triangle = new Triangle("Triangle");
         triangle.setDimensions(sideA,sideB,sideC);
-        EquilateralTriangle EQT = new EquilateralTriangle("EQT");
+        EquilateralTriangle EQT = new EquilateralTriangle("Equilateral triangle");
         EQT.setDimensions(eside);
+        Shape shape = new Shape("Anonymous Shape");
 
+        System.out.println(circle.getName());
+        circle.printDimensions();
+        System.out.println("Area: "+circle.getArea());
 
+        System.out.println(square.getName());
+        square.printDimensions();
+        System.out.println("Area: "+square.getArea());
 
+        System.out.println(triangle.getName());
+        triangle.printDimensions();
+        System.out.println("Area: "+triangle.getArea());
+
+        System.out.println(EQT.getName());
+        EQT.printDimensions();
+        System.out.println("Area: "+EQT.getArea());
+
+        System.out.println(shape.getName());
+        shape.printDimensions();
+        System.out.println("Area: "+shape.getArea());
 
     }
 }
